233fc69e212376e177ecdf92b7703e1d18974019
Quang <qxhmorrow@gmail.com>
2020-09-18 23:32:26 -0500

    char array

--- drill2.cpp
+++ drill2.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 
 int main(){
-	int count=0;
+/*	int count=0;
 	while(1>0){
 		int* a=new int[1]{3};
 		std::cout<<std::to_string(++count)+" ";
+	}
+*/
+	char c;
+	char tracker;
+	int index=0;
+	char* c_array=new char[3000];
+	while(c!='!'){
+		std::cin>>c;
+		
+		c_array[index++]=c;
+	}
+	for(int i=0;i<index;i++){
+		std::cout<<c_array[i];
 	}
 }
